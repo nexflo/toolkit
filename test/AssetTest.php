f3adbbe107395d08142bf4ed784c6ed27145243e
Bastian Allgeier <bastian.allgeier@gmail.com>
2013-04-03 17:23:54 +0200

    dummy tests for asset und exif

--- test/AssetTest.php
+++ test/AssetTest.php
@@ -3,5 +3,9 @@
 require_once('lib/bootstrap.php');
 
 class AssetTest extends PHPUnit_Framework_TestCase {
+
+  public function test() {
+    
+  }
  
 }
--- test/ExifTest.php
+++ test/ExifTest.php
@@ -3,5 +3,9 @@
 require_once('lib/bootstrap.php');
 
 class ExifTest extends PHPUnit_Framework_TestCase {
+
+  public function test() {
+    
+  }
  
 }
